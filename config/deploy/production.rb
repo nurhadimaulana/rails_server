4a6b355425a6bf9e6abc57f00401d36cb80ebcf7
Nurhadi Maulana <hadi@41studio.com>
2015-05-27 14:16:42 +0700

    Set up capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :stage, :staging
 set :rails_env, 'production'
 
-role :app, "128.199.112.33"
-role :web, "128.199.112.33"
-role :db,  "128.199.112.33", :primary => true
+# role :app, "128.199.112.33"
+# role :web, "128.199.112.33"
+# role :db,  "128.199.112.33", :primary => true
 
-set :server_name, 'http://128.199.112.33/'
+set :server_name, '128.199.112.33'
 set :rvm_ruby_version, '2.1.0'
 
 # http://stackoverflow.com/questions/21036175/how-to-deploy-a-specific-revision-with-capistrano-3
