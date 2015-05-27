1fb87b43e60979ca00382b4d0c395a9979ee7788
Nurhadi Maulana <hadi@41studio.com>
2015-05-27 14:19:55 +0700

    Set up capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,39 @@
-set :stage, :staging
-set :rails_env, 'production'
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+role :app, %w{nurhadi@128.199.112.33}
+role :web, %w{nurhadi@128.199.112.33}
+role :db,  %w{nurhadi@128.199.112.33}
 
-# role :app, "128.199.112.33"
-# role :web, "128.199.112.33"
-# role :db,  "128.199.112.33", :primary => true
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+server '128.199.112.33', user: 'nurhadi', roles: %w{web app}, my_property: :my_value
 
-set :server_name, '128.199.112.33'
-set :rvm_ruby_version, '2.1.0'
-
-# http://stackoverflow.com/questions/21036175/how-to-deploy-a-specific-revision-with-capistrano-3
-set :branch, 'master'
-
-server 'http://128.199.112.33/', user: 'nurhadi', roles: %w{web app db}
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
+# setting per server overrides global ssh_options
